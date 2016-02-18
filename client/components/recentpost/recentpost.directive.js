df99192f6a97088438755e8996e0ac79f2e10497
tourner <death.co.iis.vs@gmail.com>
2016-02-17 18:04:56 -0600

    Agregada directiva de posts recientes y mostrada en main.jade

---
+++ client/components/recentpost/recentpost.directive.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('culturaColectivaApp')
+  .directive('recentPost', function () {
+    return {
+      templateUrl: 'components/recentPost/recentPost.html',
+      restrict: 'E',
+      link: function (scope, element, attrs) {
+      }
+    };
+  });
