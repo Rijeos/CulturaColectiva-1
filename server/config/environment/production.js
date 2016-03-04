395af2085a710a741fbbd4f6154b25d96eee66a3
Rijeos <rijeos@gmail.com>
2016-03-04 16:38:24 -0600

    Update production.js
    puerto!

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -7,12 +7,12 @@ module.exports = {
   // Server port
   port:   process.env.OPENSHIFT_NODEJS_PORT ||
           process.env.PORT ||
-          31995,
+          31033,
 
   // Sequelize connecton opions
   sequelize: {
     database:'db_culturacolectiva',
     user: 'cc',
-    password:'CC-dvlpmnt'
+    password:'CCdvlpmnt'
   }
 };
